ccd0d95ae5015334a71eb9c0194cf8ff5e334880
JesseVolstruis <2597626@students.wits.ac.za>
2025-06-03 21:16:12 +0200

    Added audio recorder

---
+++ Assets/Scripts/MicManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MicManager : MonoBehaviour
+{
+    [SerializeField]
+     AudioSource audioSource;
+     int recordDuration = 10;
+     int sampleRate = 16000; 
+     string microphoneName;
+    public AudioClip audioClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartRecording()
+    {
+        string device = Microphone.devices[0];
+        audioClip = Microphone.Start(device, true, recordDuration, sampleRate);
+    }
+
+    public void PlayRecording()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public void StopRecording()
+    {
+        Microphone.End(null);
+    }
+}
